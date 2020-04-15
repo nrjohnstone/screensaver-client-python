d870adc6afb37f2c6d7938d1e1638f0272836288
nrjohnstone <locotewz@gmail.com>
2020-04-15 22:26:27 +0200

    Add service definition for screensaver and copy to systemd directory

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo mkdir /opt/screensaver-client-python
+
+sudo rm /opt/screensaver-client-python/*.* -f
+
+sudo cp screensaver.py /opt/screensaver-client-python/
+sudo cp screensaver.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/screensaver.service
+
