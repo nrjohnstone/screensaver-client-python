a19150ff538b6c7fbb7cd7921aeb4fdf52054195
nrjohnstone <locotewz@gmail.com>
2020-04-18 10:18:51 +0200

    Fix configuration replacements

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ sudo cp config.template.yml /etc/screensaver/config.yml
 
 sudo sed -i "s|<SCREENSAVER_API_HOST>|$SCREENSAVER_API_HOST|g" /etc/screensaver/config.yml
 sudo sed -i "s|<SCREENSAVER_API_PORT>|$SCREENSAVER_API_PORT|g" /etc/screensaver/config.yml
-sudo sed -i "s|<SCREEN_SAVER_CLIENT_ID>|SCREEN_SAVER_CLIENT_ID|g" /etc/screensaver/config.yml
-sudo sed -i "s|<SCREENSAVER_DISPLAY_TIME>|SCREENSAVER_DISPLAY_TIME|g" /etc/screensaver/config.yml
-sudo sed -i "s|<SCREENSAVER_FULL_SCREEN>|SCREENSAVER_FULL_SCREEN|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREEN_SAVER_CLIENT_ID>|$SCREENSAVER_CLIENT_ID|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREENSAVER_DISPLAY_TIME>|$SCREENSAVER_DISPLAY_TIME|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREENSAVER_FULL_SCREEN>|$SCREENSAVER_FULL_SCREEN|g" /etc/screensaver/config.yml
 
 #sudo cp screensaver.service /lib/systemd/system/
 #sudo chmod 644 /lib/systemd/system/screensaver.service
