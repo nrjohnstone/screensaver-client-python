b55f361f4449061698b16f7768c02fa85846d3da
nrjohnstone <locotewz@gmail.com>
2020-04-18 07:55:56 +0200

    Disable installing screensaver as a service

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo mkdir /opt/screensaver-client-python
 sudo rm /opt/screensaver-client-python/*.* -f
 
 sudo cp screensaver.py /opt/screensaver-client-python/
-sudo cp screensaver.service /lib/systemd/system/
-sudo chmod 644 /lib/systemd/system/screensaver.service
+#sudo cp screensaver.service /lib/systemd/system/
+#sudo chmod 644 /lib/systemd/system/screensaver.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable screensaver.service
+#sudo systemctl daemon-reload
+#sudo systemctl enable screensaver.service
