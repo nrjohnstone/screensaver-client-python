245cd5e79c7902db51c095957a991e0a483b6d16
nrjohnstone <locotewz@gmail.com>
2020-04-18 09:57:18 +0200

    Add configuration template for deployments

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@ sudo mkdir /opt/screensaver-client-python
 sudo rm /opt/screensaver-client-python/*.* -f
 
 sudo cp screensaver.py /opt/screensaver-client-python/
+sudo cp config.template.yml /etc/screensaver/config.yml
+
+sudo sed -i "s|<SCREENSAVER_API_HOST>|$SCREENSAVER_API_HOST|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREENSAVER_API_PORT>|$SCREENSAVER_API_PORT|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREEN_SAVER_CLIENT_ID>|SCREEN_SAVER_CLIENT_ID|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREENSAVER_DISPLAY_TIME>|SCREENSAVER_DISPLAY_TIME|g" /etc/screensaver/config.yml
+sudo sed -i "s|<SCREENSAVER_FULL_SCREEN>|SCREENSAVER_FULL_SCREEN|g" /etc/screensaver/config.yml
+
 #sudo cp screensaver.service /lib/systemd/system/
 #sudo chmod 644 /lib/systemd/system/screensaver.service
 
--- publish.ps1
+++ publish.ps1
@@ -2,5 +2,6 @@ New-Item -ItemType Directory -Force -Path ./artifacts
 Remove-Item .\artifacts\*.*
 
 Copy-Item .\screensaver.py .\artifacts
+Copy-Item .\config.template.yml .\artifacts
 Copy-Item .\screensaver.service .\artifacts
 Copy-Item .\install.sh .\artifacts
